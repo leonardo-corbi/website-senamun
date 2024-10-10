7776222749faf2c777b27a5ddf8a0e5f06e2b716
leonardoccorbi <leocesarcorbi@gmail.com>
2024-10-10 16:58:01 -0300

    ajustando comites

--- src/app/comites/dataComites.js
+++ src/app/comites/dataComites.js
@@ -1,108 +1,135 @@
 const dadosComites = [
-    {
-        tema: "Estratégias Contra a Violência e a Violação aos Direitos Humanos no Futebol",
-        comite: "Direitos Humanos",
-        modalidade: "Dupla",
-        classroom: "https://classroom.google.com/c/NjgxNjE4MDA0MzQ3?cjc=i72nikf",
-        idioma: "ptbr",
-        imagem: "HR_Futebol.jpg"
-    },
-    {
-        tema: "Violação dos Direitos Humanos no sistema carcerário de El Salvador",
-        comite: "Direitos Humanos",
-        modalidade: "Dupla",
-        classroom: "https://classroom.google.com/c/NjUyODI2MjQ1NTIx?cjc=na7x4q3",
-        idioma: "ptbr",
-        imagem: "HR_ElSalvador.png"
-    },
-    {
-        tema: "Medidas Contra o Narcotráfico Internacional",
-        comite: "Conselho de Segurança",
-        modalidade: "Dupla",
-        classroom: "https://classroom.google.com/c/Njc2NTk5MzA1ODU1?cjc=mwudzt2",
-        idioma: "ptbr",
-        imagem: "SC_Narcotráfico.jpg"
-    },
-    {
-        tema: "Regulação das Criptomoedas no Comércio Internacional",
-        comite: "OMC (Organização Mundial do Comércio)",
-        modalidade: "Dupla",
-        classroom: "https://classroom.google.com/c/NjgxNjE3OTM5Nzgy?cjc=wczy4do",
-        idioma: "ptbr",
-        imagem: "OMC_Cripto.jpg"
-    },
-    {
-        tema: "Mundialização e Reforma da Educação",
-        comite: "UNESCO (Organização das Nações Unidas para a Educação, a Ciência e a Cultura)",
-        modalidade: "Dupla",
-        classroom: "https://classroom.google.com/c/NjgxNjE3OTgyNTA0?cjc=gpbhakg",
-        idioma: "ptbr",
-        imagem: "UNESCO_Educação.jpg"
-    },
-    {
-        tema: "Medidas de Mitigação da Escravidão Moderna no Circuito da Moda",
-        comite: "OIT (Organização Internacional do Trabalho)",
-        modalidade: "Dupla",
-        classroom: "https://classroom.google.com/c/Njc2NTk5Mjg3MDg0?cjc=bhxtemp",
-        idioma: "ptbr",
-        imagem: "OIT_Moda.jpg"
-    },
-    {
-        tema: "Possibilidades de Reparação Histórica no Haiti",
-        comite: "ECOSOC (Conselho Econômico e Social das Nações Unidas)",
-        modalidade: "Dupla",
-        classroom: "https://classroom.google.com/c/NjUyODI2MjcxNDg8?cjc=f5dobyq",
-        idioma: "ptbr",
-        imagem: "ECOSOC_Haiti.png"
-    },
-    {
-        tema: "Discutindo a Preservação e Demarcação das Terras Indígenas na América",
-        comite: "OEA (Organização dos Estados Americanos)",
-        modalidade: "Dupla",
-        classroom: "https://classroom.google.com/c/NjgxNjE3OTU5NDkz?cjc=hs2emhn",
-        idioma: "ptbr",
-        imagem: "OEA_Indígenas.png"
-    },
-    {
-        tema: "Impactos da exploração de recursos naturais nos países africanos",
-        comite: "PNUMA (Programa das Nações Unidas para o Meio Ambiente)",
-        modalidade: "Dupla",
-        classroom: "https://classroom.google.com/c/NjUyODI2Mzk5MDEy?cjc=iis74fj",
-        idioma: "ptbr",
-        imagem: "PNUMA_África.jpeg"
-    },
-    {
-        tema: "Regulação do uso de feitiços e magias no mundo mágico da Disney",
-        comite: "Comitê Especial - Disney",
-        modalidade: "Individual",
-        classroom: "https://classroom.google.com/c/Njc2NTk4OTE3NjEw?cjc=6rw33ar",
-        idioma: "ptbr",
-        imagem: "Especial_Disney.jpg"
-    },
-    {
-        tema: "The Impacts of the Arctic Militarization",
-        comite: "Security Council",
-        modalidade: "Double",
-        classroom: "https://classroom.google.com/c/NjUyODI2NjAyMTM8?cjc=ogc5xrl",
-        idioma: "en",
-        imagem: "SC_ArcticMilitarization.jpg"
-    },
-    {
-        tema: "Discussing the Aftermaths and the Future of the Korean War",
-        comite: "Historical Security Council",
-        modalidade: "Double",
-        classroom: "https://classroom.google.com/c/NjgxNjE3Nzg5NDg8?cjc=fxq2bf7",
-        idioma: "en",
-        imagem: "HSC_KoreanWar.jpg"
-    },
-    {
-        tema: "The different consequences that are given to each Scuderia by FIA",
-        comite: "Special Committee - FIA",
-        modalidade: "Single",
-        classroom: "https://classroom.google.com/c/NjgxNjE4MzI0MzE8?cjc=4zej4lp",
-        idioma: "en",
-        imagem: "Special_FIA.jpg"
-    }
+  {
+    tema: "Estratégias Contra a Violência e a Violação aos Direitos Humanos no Futebol",
+    comite: "Direitos Humanos",
+    modalidade: "Dupla",
+    classroom: "https://classroom.google.com/c/NjgxNjE4MDA0MzQ3?cjc=i72nikf",
+    whatsapp: "https://chat.whatsapp.com/KTXEAgsedPbJATiJ7lsR1e",
+    pdf: "https://drive.google.com/file/d/1QgfwpT_Ijbe2d_pub50pK5VebZsbT39s/view",
+    idioma: "ptbr",
+    imagem: "HR_Futebol.jpg",
+  },
+  {
+    tema: "Violação dos Direitos Humanos no sistema carcerário de El Salvador",
+    comite: "Direitos Humanos",
+    modalidade: "Dupla",
+    classroom: "https://classroom.google.com/c/NjUyODI2MjQ1NTIx?cjc=na7x4q3",
+    whatsapp: "https://chat.whatsapp.com/GMtYLUsZxXP5htQ93Rlrpu",
+    pdf: null, // Sem PDF
+    idioma: "ptbr",
+    imagem: "HR_ElSalvador.png",
+  },
+  {
+    tema: "Medidas Contra o Narcotráfico Internacional",
+    comite: "Conselho de Segurança",
+    modalidade: "Dupla",
+    classroom: "https://classroom.google.com/c/Njc2NTk5MzA1ODU1?cjc=mwudzt2",
+    whatsapp: "https://chat.whatsapp.com/CjIpNTdJ38tCNQvjC3vkph",
+    pdf: null, // Sem PDF
+    idioma: "ptbr",
+    imagem: "SC_Narcotráfico.jpg",
+  },
+  {
+    tema: "Regulação das Criptomoedas no Comércio Internacional",
+    comite: "OMC (Organização Mundial do Comércio)",
+    modalidade: "Dupla",
+    classroom: "https://classroom.google.com/c/NjgxNjE3OTM5Nzgy?cjc=wczy4do",
+    whatsapp: "https://chat.whatsapp.com/B6xsZmeuyU6Hz5Os9UDRYa",
+    pdf: null, // Sem PDF
+    idioma: "ptbr",
+    imagem: "OMC_Cripto.jpg",
+  },
+  {
+    tema: "Mundialização e Reforma da Educação",
+    comite:
+      "UNESCO (Organização das Nações Unidas para a Educação, a Ciência e a Cultura)",
+    modalidade: "Dupla",
+    classroom: "https://classroom.google.com/c/NjgxNjE3OTgyNTA0?cjc=gpbhakg",
+    whatsapp: "hhttps://chat.whatsapp.com/D3akV8iCTrzHXTIsOeJ6ob",
+    pdf: "https://drive.google.com/file/u/6/d/1MEq7Xcngj7UjiVKfQLwz9UNaTcz2MGDi/view?usp=drive_link",
+    idioma: "ptbr",
+    imagem: "UNESCO_Educação.jpg",
+  },
+  {
+    tema: "Medidas de Mitigação da Escravidão Moderna no Circuito da Moda",
+    comite: "OIT (Organização Internacional do Trabalho)",
+    modalidade: "Dupla",
+    classroom: "https://classroom.google.com/c/Njc2NTk5Mjg3MDg0?cjc=bhxtemp",
+    whatsapp: "https://chat.whatsapp.com/Ew3WIYPxZrjE3MDWjSGvep",
+    pdf: null, // Sem PDF
+    idioma: "ptbr",
+    imagem: "OIT_Moda.jpg",
+  },
+  {
+    tema: "Possibilidades de Reparação Histórica no Haiti",
+    comite: "ECOSOC (Conselho Econômico e Social das Nações Unidas)",
+    modalidade: "Dupla",
+    classroom: "https://classroom.google.com/c/NjUyODI2MjcxNDg4?cjc=f5dobyq",
+    whatsapp: "https://chat.whatsapp.com/F2MACdg4hkn6HDbE7pgOa4",
+    pdf: null, // Sem PDF
+    idioma: "ptbr",
+    imagem: "ECOSOC_Haiti.png",
+  },
+  {
+    tema: "Discutindo a Preservação e Demarcação das Terras Indígenas na América",
+    comite: "OEA (Organização dos Estados Americanos)",
+    modalidade: "Dupla",
+    classroom: "https://classroom.google.com/c/NjgxNjE3OTU5NDkz?cjc=hs2emhn",
+    whatsapp: "https://chat.whatsapp.com/DMBG1M33N0z1luneTN4ps2",
+    pdf: "https://drive.google.com/file/u/6/d/1T-HWJjGJ7uX4xcFnYGsBJv6QswBOHXR1/view?usp=sharing",
+    idioma: "ptbr",
+    imagem: "OEA_Indígenas.png",
+  },
+  {
+    tema: "Impactos da exploração de recursos naturais nos países africanos",
+    comite: "PNUMA (Programa das Nações Unidas para o Meio Ambiente)",
+    modalidade: "Dupla",
+    classroom: "https://classroom.google.com/c/NjUyODI2Mzk5MDEy?cjc=iis74fj",
+    whatsapp: "https://chat.whatsapp.com/E4MLyv6btyg1q64hyOwhUF",
+    pdf: null, // Sem PDF
+    idioma: "ptbr",
+    imagem: "PNUMA_África.jpeg",
+  },
+  {
+    tema: "Regulação do uso de feitiços e magias no mundo mágico da Disney",
+    comite: "Comitê Especial - Disney",
+    modalidade: "Individual",
+    classroom: "https://classroom.google.com/c/Njc2NTk4OTE3NjEw?cjc=6rw33ar",
+    whatsapp: "https://chat.whatsapp.com/EfFi9dzCk5x1k9oGRFO37I",
+    pdf: "https://drive.google.com/file/u/6/d/14tYHTaZs8KNI40CzfsBPE8-5c1bU-uxI/view?usp=drive_link",
+    idioma: "ptbr",
+    imagem: "Especial_Disney.jpg",
+  },
+  {
+    tema: "The Impacts of the Arctic Militarization",
+    comite: "Security Council",
+    modalidade: "Double",
+    classroom: "https://classroom.google.com/c/NjUyODI2NjAyMTM8?cjc=ogc5xrl",
+    whatsapp: "https://chat.whatsapp.com/FeK1tjPPhLQECFgVfQkt2h",
+    pdf: "https://drive.google.com/file/u/6/d/1Tx9W9NAUsOBsYbHZqFs1vGrq2rKIqoPr/view?usp=sharing",
+    idioma: "en",
+    imagem: "SC_ArcticMilitarization.jpg",
+  },
+  {
+    tema: "Discussing the Aftermaths and the Future of the Korean War",
+    comite: "Historical Security Council",
+    modalidade: "Double",
+    classroom: "https://classroom.google.com/c/NjgxNjE3Nzg5NDg8?cjc=fxq2bf7",
+    whatsapp: "https://chat.whatsapp.com/EHLLPQjzBjk7wiKunObRIN",
+    pdf: "https://drive.google.com/file/u/6/d/1GhzE5g_-NEZq5JTjxAWjbindIFyHIq3p/view?usp=drive_link",
+    idioma: "en",
+    imagem: "HSC_KoreanWar.jpg",
+  },
+  {
+    tema: "The different consequences that are given to each Scuderia by FIA",
+    comite: "Special Committee - FIA",
+    modalidade: "Single",
+    classroom: "https://classroom.google.com/c/NjgxNjE4MzI0MzE8?cjc=4zej4lp",
+    whatsapp: "https://chat.whatsapp.com/GRnP77c8iBf9il6NbR9DrW",
+    pdf: "https://drive.google.com/file/u/6/d/1E3umNYkY7JPchlOD_KCzkTuEuqOgfxEJ/view?usp=drive_link",
+    idioma: "en",
+    imagem: "Special_FIA.jpg",
+  },
 ];
 
 export default dadosComites;
--- src/app/comites/page.tsx
+++ src/app/comites/page.tsx
@@ -1,64 +1,169 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import Link from "next/link";
+import dados from "./dataComites"; 
 import HomepageHeader from "@/components/HomepageHeader";
 
+const blurBackground = "backdrop-blur-md";
+
+interface Committee {
+  tema: string;
+  comite: string;
+  modalidade: string;
+  classroom: string;
+  whatsapp: string;
+  pdf: string | null;
+  idioma: string;
+  imagem: string | null;
+}
+
 export default function ComitesPage() {
-  const headerHeight = 80;
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalData, setModalData] = useState<Committee | null>(null);
+  const [committees, setCommittees] = useState<Committee[]>([]);
+
+  useEffect(() => {
+    setCommittees(dados);
+  }, []);
+
+  const openModal = (item: Committee) => {
+    setModalData(item);
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
 
   return (
     <div>
-      <HomepageHeader pageTitle="Comitês" pageSubtitle="Descubra os temas" />
-      <section className="bg-blue-custom h-auto xl:h-screen flex flex-col justify-center">
-        <div className="flex flex-col md:flex-row justify-center items-stretch xl:px-32 py-12 xl:py-32">
-          {/* Card 1 */}
-          <div className="flex flex-col p-8 border-2 rounded-2xl shadow-md mx-4 mb-8 md:mb-0 md:flex-grow">
-            <h1 className="lg:text-5xl text-3xl text-yellow-custom font-semibold">
-              Comitês SenaMUN 2024
-            </h1>
-            <p className="text-white lg:text-xl text-lg mb-4 font-medium text-nowrap">
-              em Português
-            </p>
-            <ul className="list-disc list-inside mb-4 text-white lg:text-xl text-lg flex-grow">
-              <li>Direitos Humanos (2)</li>
-              <li>Conselho de Segurança</li>
-              <li>OMC</li>
-              <li>UNESCO</li>
-              <li>OIT</li>
-              <li>ECOSOC</li>
-              <li>OEA</li>
-              <li>CMA</li>
-              <li>Especial</li>
-            </ul>
-            <Link
-              href="/comites/ptbr"
-              className="py-2 px-4 rounded-2xl border-2 border-yellow-custom text-white hover:bg-yellow-custom hover:text-lg duration-300 text-center block"
-            >
-              Saiba mais
-            </Link>
+      <HomepageHeader pageTitle="Comitês" pageSubtitle="Lista completa" />
+      <div className="flex flex-col bg-gray-100 min-h-fit p-6">
+        {committees.map((committee, index) => (
+          <div
+            key={index}
+            className="bg-white rounded-lg shadow-xl mb-8 flex flex-col lg:flex-row h-auto max-w-full"
+          >
+            <div className="w-full lg:w-1/3">
+              <Image
+                src={
+                  committee.imagem
+                    ? `/comites/${committee.imagem}`
+                    : "/logo-senamun.svg"
+                }
+                alt="Imagem do Comitê"
+                width={800}
+                height={500}
+                className="object-cover rounded-lg w-full h-64 lg:h-72"
+              />
+            </div>
+            <div className="ml-0 lg:ml-6 w-full lg:w-2/3 p-4 lg:p-6">
+              <h2 className="text-2xl font-bold">{committee.tema}</h2>
+              <p className="mt-2 text-lg font-semibold">{committee.comite}</p>
+              <p className="mt-2">Modalidade: {committee.modalidade}</p>
+              <p className="mt-2">Idioma: {committee.idioma.toUpperCase()}</p>
+              <button
+                onClick={() => openModal(committee)}
+                className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+              >
+                Ver detalhes
+              </button>
+            </div>
           </div>
+        ))}
+      </div>
 
-          {/* Card 2 */}
-          <div className="flex flex-col p-8 border-2 rounded-2xl shadow-md mx-4 md:flex-grow">
-            <h1 className="lg:text-5xl text-3xl text-yellow-custom font-semibold">
-              Comitês SenaMUN 2024
-            </h1>
-            <p className="text-white lg:text-xl text-lg mb-4 font-medium text-nowrap ">
-              em Inglês
-            </p>
-            <ul className="list-disc list-inside mb-4 text-white lg:text-xl text-lg flex-grow">
-              <li>SC</li>
-              <li>HSC</li>
-              <li>Special</li>
-            </ul>
-            <Link
-              href="/comites/en"
-              className="py-2 px-4 rounded-2xl border-2 border-yellow-custom text-white hover:bg-yellow-custom hover:text-lg duration-300 text-center block"
+      {isOpen && modalData && (
+        <div
+          className={`fixed z-10 inset-0 overflow-y-auto ${
+            isOpen ? blurBackground : ""
+          }`}
+        >
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div
+              className="fixed inset-0 transition-opacity"
+              aria-hidden="true"
             >
-              Saiba mais
-            </Link>
+              <div className="inset-0 bg-gray-500 opacity-75"></div>
+            </div>
+            <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
+            <div
+              className={`${
+                isOpen
+                  ? "opacity-100 transition-opacity duration-200"
+                  : "opacity-0 transition-opacity duration-300"
+              } inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full`}
+              role="dialog"
+              aria-modal="true"
+              aria-labelledby="modal-headline"
+            >
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <h3
+                      className="text-lg leading-6 font-medium text-gray-900"
+                      id="modal-headline"
+                    >
+                      {modalData.tema}
+                    </h3>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        Comitê: {modalData.comite}
+                      </p>
+                      <p className="text-sm text-gray-500">
+                        Modalidade: {modalData.modalidade}
+                      </p>
+                      <p className="text-sm text-gray-500">
+                        Idioma: {modalData.idioma.toUpperCase()}
+                      </p>
+
+                      {/* Links em Grid */}
+                      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-4">
+                        <a
+                          href={modalData.classroom}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                        >
+                          Classroom
+                        </a>
+                        <a
+                          href={modalData.whatsapp}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+                        >
+                          WhatsApp
+                        </a>
+                        {modalData.pdf && (
+                          <a
+                            href={modalData.pdf}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+                          >
+                            PDF
+                          </a>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <button
+                  type="button"
+                  className="mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                  onClick={closeModal}
+                >
+                  Fechar
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-      </section>
+      )}
     </div>
   );
 }
